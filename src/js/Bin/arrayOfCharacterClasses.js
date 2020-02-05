46aec143f7e3f3c36dfe8899556efe97048eaee0
Мишутушкин Максим Александрович <ma.mishutushkin@severstal.com>
2020-02-05 09:37:35 +0300

    nonUsedFile

--- src/js/Bin/arrayOfCharacterClasses.js
+++
@@ -1,8 +0,0 @@
-import Bowman from './Bowman';
-import Daemon from './Daemon';
-import Magician from './Magician';
-import Swordsman from './Swordsman';
-import Undead from './Undead';
-import Vampire from './Vampire';
-
-export default [Bowman, Swordsman, Magician, Undead, Vampire, Daemon];
